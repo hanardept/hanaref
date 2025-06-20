c48b7cb136087689a080a50268faa71462856185
hanardept@gmail.com <hanardept@gmail.com>
2025-06-20 17:22:25 +0300

    Add backend url as env var (distinguish between preview & live versions)

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/redux-logic';
 
+console.log(`backend url: ${process.env.REACT_APP_BACKEND_URL}`);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
