99c8f60f5e9cbf83569695d96b8591e278a69684
Yonatan D <yonatand80@gmail.com>
2026-02-18 10:15:32 +0200

    Fixed typo

--- src/components/item-page/ItemPage.tsx
+++ src/components/item-page/ItemPage.tsx
@@ -143,7 +143,7 @@ const ItemPage = () => {
                     </>
                 }
                 </p>
-                {item.price !=== null && item.price !== undefined && <p>{item.price}</p>}
+                {item.price !== null && item.price !== undefined && <p>{item.price}</p>}
                 {item.description && <p>{item.description}</p>}
                 {item.imageLink && <img src={item.imageLink} alt={item.name} />}
                 {
