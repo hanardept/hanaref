fbb2c02bd1227070c7e341be74053ec2025a217b
hanardept <111859599+hanardept@users.noreply.github.com>
2026-04-02 17:16:43 +0300

    Add files via upload

---
+++ src/components/item-search/SupplierSearchPage.tsx
@@ -0,0 +1,13 @@
+import Suppliers from "./Suppliers";
+import SupplierSearchMenu from "./SupplierSearchMenu";
+
+const SupplierSearchPage = () => {
+    return (
+        <>
+            <SupplierSearchMenu />
+            <Suppliers />
+        </>
+    );
+};
+
+export default SupplierSearchPage;
